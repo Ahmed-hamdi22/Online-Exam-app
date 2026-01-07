1ce80532dad141d5f84cee27e6050103a7c5e0da
Ahmed-hamdi22 <ahmed255hamdi@gmail.com>
2026-01-07 17:39:50 +0200

    refactor(auth): improve register flow

--- src/app/[locale]/(auth)/register/_components/register.form.tsx
+++ src/app/[locale]/(auth)/register/_components/register.form.tsx
@@ -1,293 +1,207 @@
-"use client";
+'use client';
 
-import FeedbackMessage from "@/components/common/feedback-message";
-import { Button } from "@/components/ui/button";
-import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { useRouter } from "@/i18n/routing";
-import { registerAction } from "@/lib/actions/auth.action";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Eye, EyeOff } from "lucide-react";
-import { signIn } from "next-auth/react";
-import { useTranslations } from "next-intl";
-import Link from "next/link";
-import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 
+import { registerSchema, registerValues } from '@/lib/schemes/authschema';
+import { Input } from '@/components/ui/input';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import FeedBack from '@/components/shared/feedback';
+import { Loader } from 'lucide-react';
+import { PhoneInput } from '@/components/ui/phone-input';
+import { useRouter } from 'next/navigation';
+import useRegister from '../_hook/use-register';
 export default function RegisterForm() {
-  const t = useTranslations();
   const router = useRouter();
-
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
-
-  const Schema = z
-    .object({
-      username: z.string().min(2, t("username-min")),
-      firstName: z.string().min(2, t("firstname-min")),
-      lastName: z.string().min(2, t("lastname-min")),
-      email: z.string().min(1, t("email-required")).email(t("email-invalid")),
-      phone: z.string().min(1, t("phone-required")),
-      password: z.string().min(8, t("password-is-too-short")),
-      rePassword: z.string().min(1, t("password-confirm-required")),
-    })
-    .refine((values) => values.password === values.rePassword, {
-      message: t("password-confirm-mismatch"),
-      path: ["rePassword"],
-    });
-
-  type Inputs = z.infer<typeof Schema>;
-
-  const form = useForm<Inputs>({
-    resolver: zodResolver(Schema),
+  const form = useForm<registerValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+      rePassword: '',
+      firstName: '',
+      lastName: '',
+      phone: '',
+      username: '',
+    },
+    resolver: zodResolver(registerSchema),
   });
 
-  const onSubmit: SubmitHandler<Inputs> = async (values) => {
-    setError(null);
-    setLoading(true);
+  // Mutation
+  const { isPending, error, register } = useRegister();
 
-    const response = await registerAction(values);
+  //
+  const onSubmit: SubmitHandler<registerValues> = (values) => {
+    register(values, {
+      onSuccess: () => {
+        router.push('/login');
+      },
 
-    setLoading(false);
-
-    if (response.message === "success") {
-      form.reset();
-      router.push("/auth/login");
-      return;
-    }
-
-    if (Array.isArray(response.message)) {
-      response.message.forEach((error) => {
-        form.setError(error.field as keyof Inputs, {
-          message: error.errorMessage,
-          type: "invalid",
-        });
-      });
-    } else {
-      setError(response.message);
-    }
+      onError: (err: unknown) => {
+        console.log(err);
+      },
+    });
+    console.log('values', values);
   };
 
   return (
-    <div className="flex flex-col gap-8 justify-center items-center w-full">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col w-full gap-4"
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full max-w-md grid graid-cols-2 gap-4"
+      >
+        {/* Frirst Name*/}
+        <FormField
+          control={form.control}
+          name="firstName"
+          render={({ field }) => (
+            <FormItem className="mb-4">
+              <FormLabel>First Name</FormLabel>
+
+              <FormControl>
+                <Input placeholder="Enter your first name" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Last Name */}
+        <FormField
+          control={form.control}
+          name="lastName"
+          render={({ field }) => (
+            <FormItem className="mb-4">
+              <FormLabel>Last Name</FormLabel>
+
+              <FormControl>
+                <Input placeholder="Enter your last name" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* User Name */}
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className=" col-span-2">
+              <FormLabel>User Name</FormLabel>
+
+              <FormControl>
+                <Input placeholder="Enter your user name" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Email */}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className=" col-span-2">
+              <FormLabel>Email</FormLabel>
+
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Phone */}
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem className=" col-span-2">
+              <FormLabel>Phone</FormLabel>
+
+              <FormControl>
+                <PhoneInput
+                  placeholder="Enter your phone number"
+                  {...field}
+                  countries={['EG']}
+                  international
+                  defaultCountry="EG"
+                />
+                {/*  Selcet Country is disabled by default in Phone Input  CountrySelectProps >> disabled: true  line 75*/}
+              </FormControl>
+              <FormDescription className="italic">
+                Only Egyptian numbers are allowed{' '}
+              </FormDescription>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* PassWord */}
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem className=" col-span-2">
+              <FormLabel>Password</FormLabel>
+
+              <FormControl>
+                <Input type="password" placeholder="******" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/*Confirm PassWord */}
+        <FormField
+          control={form.control}
+          name="rePassword"
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <FormLabel>Confirm Password</FormLabel>
+
+              <FormControl>
+                <Input type="password" placeholder="******" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* FeedBack */}
+        <FeedBack className="mt-4 col-span-2 ">{error?.message}</FeedBack>
+
+        {/* Submit */}
+        <Button
+          disabled={
+            isPending ||
+            (form.formState.isSubmitting && !form.formState.isValid)
+          }
+          className="col-span-2 mt-10 text-white w-full bg-blue-600 hover:bg-blue-700 "
+          type="submit"
         >
-          <h3 className="text-2xl font-semibold mb-4">{t("sign-up")}</h3>
-
-          {/* Username */}
-          <FormField
-            name="username"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <Input
-                  {...field}
-                  placeholder={t("username-placeholder")}
-                  className="rounded-2xl h-10 border border-gray-300 focus:border-[#3366b8] focus:ring-0"
-                />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          {/* First Name */}
-          <FormField
-            name="firstName"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <Input
-                  {...field}
-                  placeholder={t("firstname-placeholder")}
-                  className="rounded-2xl h-10 border border-gray-300 focus:border-[#3366b8] focus:ring-0"
-                />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          {/* Last Name */}
-          <FormField
-            name="lastName"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <Input
-                  {...field}
-                  placeholder={t("lastname-placeholder")}
-                  className="rounded-2xl h-10 border border-gray-300 focus:border-[#3366b8] focus:ring-0"
-                />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          {/* Email */}
-          <FormField
-            name="email"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <Input
-                  {...field}
-                  placeholder={t("email-placeholder")}
-                  className="rounded-2xl h-10 border border-gray-300 focus:border-[#3366b8] focus:ring-0"
-                />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          {/* Phone */}
-          <FormField
-            name="phone"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <Input
-                  {...field}
-                  placeholder={t("phone-placeholder")}
-                  className="rounded-2xl h-10 border border-gray-300 focus:border-[#3366b8] focus:ring-0"
-                />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* Password */}
-          <FormField
-            name="password"
-            control={form.control}
-            render={({ field }) => {
-              const [showPassword, setShowPassword] = useState(false);
-
-              return (
-                <FormItem className="relative">
-                  <Input
-                    {...field}
-                    type={showPassword ? "text" : "password"}
-                    placeholder={t("password-placeholder")}
-                    className="rounded-2xl h-10 border border-gray-300 focus:border-[#3366b8] focus:ring-0"
-                  />
-                  <div
-                    className="absolute bottom-2 end-3 flex items-center cursor-pointer"
-                    onClick={() => setShowPassword((prev) => !prev)}
-                  >
-                    {showPassword ? (
-                      <EyeOff className="w-5 h-5 text-gray-500" />
-                    ) : (
-                      <Eye className="w-5 h-5 text-gray-500" />
-                    )}
-                  </div>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-
-          {/* Confirm Password */}
-          <FormField
-            name="rePassword"
-            control={form.control}
-            render={({ field }) => {
-              const [showPassword, setShowPassword] = useState(false);
-
-              return (
-                <FormItem className="relative">
-                  <Input
-                    {...field}
-                    type={showPassword ? "text" : "password"}
-                    placeholder={t("register-password-confirm-placeholder")}
-                    className="rounded-2xl h-10 border border-gray-300 focus:border-[#3366b8] focus:ring-0"
-                  />
-                  <div
-                    className="absolute bottom-2  end-3 flex items-center cursor-pointer"
-                    onClick={() => setShowPassword((prev) => !prev)}
-                  >
-                    {showPassword ? (
-                      <EyeOff className="w-5 h-5 text-gray-500" />
-                    ) : (
-                      <Eye className="w-5 h-5 text-gray-500" />
-                    )}
-                  </div>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-
-          <div className="text-sm mt-1 flex  justify-center">
-            {t("already-have-account")}{" "}
-            <Link href="/login" className="text-blue-500 hover:underline">
-              {t("login")}
-            </Link>
-          </div>
-
-          <FeedbackMessage>{error}</FeedbackMessage>
-
-          <Button
-            type="submit"
-            className="w-full mt-2 bg-[#122D9C] rounded-2xl"
-            disabled={loading || !form.formState.isValid}
-          >
-            {t("register-submit")}
-          </Button>
-        </form>
-      </Form>
-
-      {/* Social Providers */}
-      <div className="text-center mt-3">
-        <p>{t("or-continue-with")}</p>
-        <div className="flex justify-center mt-4">
-          <div
-            onClick={() =>
-              signIn("google", { callbackUrl: "/dashboard/subjects" })
-            }
-            className="login-item flex justify-center items-center hover:shadow-lg border p-2 shadow-md rounded-lg cursor-pointer mx-1"
-          >
-            <img
-              className="w-5 h-5"
-              alt="google"
-              src={"/images/Logo Google.png"}
-            />
-          </div>
-          <div
-            onClick={() =>
-              signIn("facebook", { callbackUrl: "/dashboard/subjects" })
-            }
-            className="login-item flex justify-center items-center hover:shadow-lg border p-2 shadow-md rounded-lg cursor-pointer mx-1"
-          >
-            <img
-              className="w-5 h-5"
-              alt="facebook"
-              src={"/images/Vector.png"}
-            />
-          </div>
-          <div
-            onClick={() =>
-              signIn("twitter", { callbackUrl: "/dashboard/subjects" })
-            }
-            className="login-item flex justify-center items-center hover:shadow-lg border p-2 shadow-md rounded-lg cursor-pointer mx-1"
-          >
-            <img className="w-5 h-5" alt="twitter" src={"/images/Logo.png"} />
-          </div>
-          <div
-            onClick={() =>
-              signIn("github", { callbackUrl: "/dashboard/subjects" })
-            }
-            className="login-item flex justify-center items-center hover:shadow-lg border p-2 shadow-md rounded-lg cursor-pointer mx-1"
-          >
-            <img
-              className="w-5 h-5"
-              alt="github"
-              src={"/images/Logo (1).png"}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+          {isPending || form.formState.isSubmitting ? <Loader /> : 'Register'}
+        </Button>
+      </form>
+    </Form>
   );
 }
--- src/app/[locale]/(auth)/register/page.tsx
+++ src/app/[locale]/(auth)/register/page.tsx
@@ -1,8 +1,25 @@
-import RegisterForm from "./_components/register.form";
-export default function Page() {
+import Link from 'next/link';
+import React from 'react';
+import Headline from '../_components/headline';
+import RegisterForm from './_components/register.form';
+
+export default function RegisterPage() {
   return (
-    <div>
-      <RegisterForm />
-    </div>
+    <main className="flex items-center justify-center overflow-auto">
+      <div className="sm:w-full  md:w-[28.25rem] space-y-10">
+        <Headline> Create Account</Headline>
+
+        {/* Form */}
+        <RegisterForm />
+
+        {/* Link */}
+        <p className=" pb-12  md:pb-0">
+          Already have an account? &nbsp;
+          <Link className="text-blue-500 font-medium" href="/login">
+            Login{' '}
+          </Link>
+        </p>
+      </div>
+    </main>
   );
 }
